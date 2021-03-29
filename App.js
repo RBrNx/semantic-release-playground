1caab6676d4bc7d795889f6bbdb75743a6dd04e3
Conor Watson <conor@odro.co.uk>
2021-03-29 19:48:29 +0100

    chore: remove console logs

--- App.js
+++ App.js
@@ -3,10 +3,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log('chore');
-  console.log('feature');
-  console.log('another feature')
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
